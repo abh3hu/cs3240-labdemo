60a3170baccea5f2cc9884231e637cdc0d7ea198
Allen Huynh <Allen@Allens-MacBook-Pro.local>
2016-02-22 14:20:14 -0500

    adding new branch and file

--- hello.py
+++ hello.py
@@ -2,3 +2,6 @@ from helper import greeting
 if __name__ == '__main__':
 	print('hello')
 	greeting("hello there")
+    #this is a random comment
+    print("this is something in the master and not the branch")
+
